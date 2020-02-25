bd84708f759e2cdebb8601072cc8e156ece0d965
vlromaniuk <vlad96r@protonmail.com>
2020-02-25 23:43:11 +0200

    create theme

---
+++ app/design/frontend/elogic/portofonWeb/registration.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+
+elogic theme.
+@author Vladislav Romaniuk vlad96r@protonmial.com
+@copyright 2020 Vlromaniuk
+ */
+
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/elogic/portofonWeb', __DIR__);
