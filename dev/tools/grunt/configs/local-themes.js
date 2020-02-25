02b8aff51163dcb54f2c8a83686dce0c03e25e62
vlromaniuk <vlad96r@protonmail.com>
2020-02-26 00:40:54 +0200

    add fonts/color variables

---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,14 @@
+module.exports = {
+    portofon: {
+        area: 'frontend',
+        name: 'elogic/portofonWeb',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l',
+            'css/email',
+            'css/email-inline'
+        ],
+        dsl: 'less'
+    },
+};
---
+++ grunt.sh
@@ -0,0 +1,5 @@
+grunt clean:portofon;
+grunt exec:portofon;
+grunt less:portofon;
+sudo chmod -R 777 /var/www/html;
+
